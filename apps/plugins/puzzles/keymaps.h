c2fd9bd1e8be4411f7777648bea3406481bce5e2
xvortex <vsoftster@gmail.com>
2017-03-24 07:57:39 +0300

    Fix keymaps

--- apps/plugins/puzzles/keymaps.h
+++ apps/plugins/puzzles/keymaps.h
@@ -178,6 +178,30 @@
 #define BTN_FIRE       BUTTON_BOTTOMLEFT
 #define BTN_PAUSE      BUTTON_TOPLEFT
 
+#elif (CONFIG_KEYPAD == XDUOO_X3_PAD)
+#define BTN_UP           BUTTON_VOL_UP
+#define BTN_DOWN         BUTTON_VOL_DOWN
+#define BTN_LEFT         BUTTON_PREV
+#define BTN_RIGHT        BUTTON_NEXT
+#define BTN_FIRE         BUTTON_PLAY
+#define BTN_PAUSE        BUTTON_POWER
+
+#elif (CONFIG_KEYPAD == IHIFI_770_PAD)
+#define BTN_UP           BUTTON_PREV
+#define BTN_DOWN         BUTTON_NEXT
+#define BTN_LEFT         BUTTON_HOME
+#define BTN_RIGHT        BUTTON_VOL_DOWN
+#define BTN_FIRE         BUTTON_PLAY
+#define BTN_PAUSE        BUTTON_POWER
+
+#elif (CONFIG_KEYPAD == IHIFI_800_PAD)
+#define BTN_UP           BUTTON_PREV
+#define BTN_DOWN         BUTTON_NEXT
+#define BTN_LEFT         BUTTON_HOME
+#define BTN_RIGHT        BUTTON_VOL_DOWN
+#define BTN_FIRE         BUTTON_PLAY
+#define BTN_PAUSE        BUTTON_POWER
+
 #else
 #error Unsupported keypad
 #endif
